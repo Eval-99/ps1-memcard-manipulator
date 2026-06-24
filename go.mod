9985b1d58b110cb5ce70a64effedf830bf5cb344
Eval-99 <ev1914jw@gmail.com>
2026-06-23 23:23:45 -0500

    String and ShiftJIS parsing.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Eval-99/ps1-memcard-manipulator
 
 go 1.26.4
+
+require golang.org/x/text v0.38.0 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
+	"slices"
 	"strings"
+
+	"golang.org/x/text/encoding/japanese"
 )
 
 var SUPPORTED_FORMATS = [5]string{".mcr", ".bin", ".gme", ".mcd", ".srm"}
@@ -14,6 +17,8 @@ const (
 	DIR_FRAME_OFFSET = 128
 	MCS_FRAME_SIZE   = 128 + BLOCK_SIZE
 )
+
+// --- Utility ---
 
 func getFileExtension(filename string) (string, error) {
 	if len(filename) == 0 {
@@ -43,6 +48,8 @@ func changeFileExtension(filename, newExt string) (string, error) {
 	return strings.ToLower(strings.Join(strSlice, ".")), nil
 }
 
+// --- Binary Parsing ---
+
 func updateChecksum(data []byte, offset int) {
 	var x byte = 0
 	for k := range 127 {
@@ -59,18 +66,41 @@ func verifyChecksum(data []byte, offset int) bool {
 	return data[offset+127] == x
 }
 
+func parseString(data []byte, offset int, length int) string {
+	var s strings.Builder
+	for k := range length {
+		s.WriteString(string(data[offset+k]))
+	}
+	return s.String()
+}
+
+func parseShiftJIS(data []byte, offset int, length int) (string, error) {
+	sub := data[offset : offset+length]
+	end := slices.Index(sub, 0)
+	if end >= 0 {
+		sub = sub[0:end]
+	}
+	decoder := japanese.ShiftJIS.NewDecoder()
+	return decoder.String(string(sub))
+}
+
 func main() {
-	filename := "somefile.exe"
-
-	result, err := getFileExtension(filename)
-	if err != nil {
-		fmt.Println(err)
+	data := []byte{
+		130, 177,
+		130, 241,
+		130, 201,
+		130, 191,
+		130, 205,
+		129, 65,
+		130, 168,
+		140, 179,
+		139, 67,
+		130, 197,
+		130, 183,
+		130, 169,
+		129, 72,
 	}
 
-	thing, err := changeFileExtension(filename, "zip")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println(filename, result, "=======", thing)
+	japaneseString, _ := parseShiftJIS(data, 0, 26)
+	fmt.Println(japaneseString)
 }
